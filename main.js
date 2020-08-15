fd04706ab2fe4ae06958b0821477362a3ee0853d
Sijan Dahal <47341534+sijandahal@users.noreply.github.com>
2020-08-15 15:39:46 +0545

    Added close button

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const burger = document.querySelector('.burger');
+const showburger = document.querySelector('.burger__image');
+const close = document.querySelector('.close');
 const ul = document.querySelector('.list_items')
 
 burger.addEventListener('click', function(){
     ul.classList.toggle('toggle');
+    showburger.classList.toggle('toggle');
+    close.classList.toggle('toggle');
 })
